e0c476483b89c7fa01f0a4d63ff7d546a4661e6f
Cyrus Hilliard <cyrus_hilliard@comcast.com>
2016-07-18 10:53:49 -0400

    Fixed another mfloat-abi declaration

--- scripts/build.js
+++ scripts/build.js
@@ -173,7 +173,7 @@ function configure() {
   process.env['GYP_DEFINES'] += ' enable_tracing=1';
   process.env['GYP_DEFINES'] += ' include_pulse_audio=0';
   process.env['GYP_DEFINES'] += ' include_internal_audio_device=1';
-  process.env['GYP_DEFINES'] += ' arm_float_abi=softp';
+  process.env['GYP_DEFINES'] += ' arm_float_abi=softfp';
   process.env['GYP_DEFINES'] += ' arm_neon=1';
   process.env['GYP_DEFINES'] += ' werror=';
   process.env['GYP_CROSSCOMPILE'] = 1;
